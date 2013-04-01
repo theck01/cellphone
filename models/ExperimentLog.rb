7cfd840dec4dfd35e8b0315218ebba8896503f09
Tyler Heck <tyler.heck@gmail.com>
2013-04-01 12:55:47 -0400

    Update: moved database config to init.rb

--- models/ExperimentLog.rb
+++ models/ExperimentLog.rb
@@ -1,8 +1,12 @@
-
-require 'sinatra'
-require 'sinatra/activerecord'
-
-set :database, 'sqlite3:///db/experiment_logs.sq3'
-
 class ExperimentLog < ActiveRecord::Base
+  validates :experiment_name, presence: true
+  validates :drug_name, presence: true
+  validates :drug_ul_dose, presence: true
+  validates :drug_ul_dose, presence: true
+
+  # overriden create method adds application time to params hash
+  def ExperimentLog.create(params)
+    params[:application_time] = Time.now
+    super(params)
+  end
 end
--- models/init.rb
+++ models/init.rb
@@ -1,1 +1,6 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+
+set :database, 'sqlite3:///db/experiment_logs.sq3'
+
 require './models/ExperimentLog'
