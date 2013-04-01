9acefae264c5ba95d720efcb992b1e304057acb9
Tyler Heck <tyler.heck@gmail.com>
2013-04-01 12:58:25 -0400

    Add: RMagick gem and image histogram methods to imagecapture helper

--- Gemfile
+++ Gemfile
@@ -3,13 +3,14 @@ source 'https://rubygems.org'
 gem 'activerecord'
 gem 'haml'
 gem 'json'
+gem 'rmagick'
 gem 'sinatra'
 gem 'sinatra-activerecord'
 gem 'sinatra-contrib'
 gem 'sqlite3'
 gem 'thin'
 
-group :development do
-  gem 'shotgun'
+group :development, :test do
+  gem 'pry'
   gem 'tux'
 end
--- helpers/imagecapture.rb
+++ helpers/imagecapture.rb
@@ -1,8 +1,10 @@
 require 'sinatra/base'
+require 'RMagick'
+include Magick
 
 module Sinatra
   module ImageCapture
-    def get_image (name="image",path=".")
+    def get_image (name,path=".")
 
       # drop final / in path if it exists
       path = path[0..-2] if path[-1] == '/'
@@ -13,6 +15,26 @@ module Sinatra
       # run image capture program
       `lib/TWAINCom #{image_path} -fjpg -grayscale -h -o`
       image_path
+    end
+
+
+    def get_histogram (name, path='.')
+
+      # drop final / in path if it exists
+      path = path[0..-2] if path[-1] == '/'
+
+      image_name = "#{name}#{DateTime.now.strftime("_%Hh%Mm%Ss_%b-%d-%Y")}.jpg"
+      image_path = "#{path}/#{image_name}"
+
+      # load image
+      pixels = ImageList.new(image_path).export_pixels
+
+      # bin image intensities, reducing 16bit image to 8bit values
+      histogram = Array.new(256,0)
+      pixels.each{ |i| histogram[i/256] += 1 }
+
+      # normalize pixel intensities
+      histogram.map{ |i| i.to_f/pixels.length }
     end
   end
 
