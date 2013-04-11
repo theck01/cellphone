5359b09843e5be62aa021f683d9ab14013c95498
Tyler Heck <tyler.heck@gmail.com>
2013-04-11 10:30:41 -0400

    histogram route no longer redirects

--- routes/api.rb
+++ routes/api.rb
@@ -1,3 +1,5 @@
+require 'restclient'
+
 class AutoExpApp < Sinatra::Base
 
   # request dosage and syringe information as json
@@ -8,7 +10,19 @@ class AutoExpApp < Sinatra::Base
 
   # request the histogram of the last saved image
   get '/api/histogram' do
-    redirect "http://#{settings.histogrammer_ip}:3000/api/histogram/#{@@recent_img_name}"
+    content_type :json
+    begin
+      response = RestClient.get "http://#{settings.histogrammer_ip}:3000/api/histogram/#{@@recent_img_name}"
+      File.open("./public/assets/histograms/histogram_of_#{@@recent_img_name}", "w") do |file|
+        file.puts response.body
+      end
+      response.body
+    rescue
+      File.open("./public/assets/histograms/.histogram_of_#{@@recent_img_name}") do |file|
+        prev_hist = file.gets
+      end
+      prev_hist
+    end
   end
 
   # request name of most recent image captured from the microscope
